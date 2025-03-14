d856850f5382c2917f2ba1996485aa920c7693e1
Azhar Shah <azharshahsaintlouis@gmail.com>
2025-03-14 12:52:59 +0000

    chore: add font for home page

--- family-recipes/src/components/home/Home.tsx
+++ family-recipes/src/components/home/Home.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <div className="bg-[url(../../public/paper.jpg)] h-screen">Hello</div>;
+  return (
+    <div className="bg-[url(../../public/paper.jpg)] h-screen italianno-regular">
+      Family Recipes
+    </div>
+  );
 };
 
 export default Home;
