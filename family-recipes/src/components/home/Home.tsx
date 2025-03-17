71ba659f468c16c2e807e8d52e0917e6cc70a2af
Azhar Shah <azharshahsaintlouis@gmail.com>
2025-03-17 18:13:53 +0000

    chore: add responsive design and link

--- family-recipes/src/components/home/Home.tsx
+++ family-recipes/src/components/home/Home.tsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div className="bg-[url(../../public/paper.jpg)] w-full h-lvh italianno-regular">
-      Family Recipes
+    <div className="flex h-lvh w-full items-center justify-center bg-[url(../../public/paper.jpg)]">
+      <div className="italianno-sm sm:text-9xl">Family Recipes</div>
+      <Link
+        className="sheen w-4.5 rotate-130 relative left-[-333px] top-[-32px] h-3.5 rounded-[50%] bg-black text-[0px] hover:shadow sm:left-[-445px] sm:top-[-38px]"
+        to="#"
+      >
+        link
+      </Link>
+      <button className="bg-white hover:shadow">hello</button>
     </div>
   );
 };
