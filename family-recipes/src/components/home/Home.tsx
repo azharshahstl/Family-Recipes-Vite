779e511cf8b801a18ffa2adb3b9c2e8b715160ef
Azhar Shah <azharshahsaintlouis@gmail.com>
2025-03-18 09:39:01 +0000

    chore: make link position responsive

--- family-recipes/src/components/home/Home.tsx
+++ family-recipes/src/components/home/Home.tsx
@@ -5,12 +5,11 @@ const Home = () => {
     <div className="flex h-lvh w-full items-center justify-center bg-[url(../../public/paper.jpg)]">
       <div className="italianno-sm sm:text-9xl">Family Recipes</div>
       <Link
-        className="sheen w-4.5 rotate-130 relative left-[-333px] top-[-32px] h-3.5 rounded-[50%] bg-black text-[0px] hover:shadow sm:left-[-445px] sm:top-[-38px]"
+        className="sheen relative left-[-333px] top-[-32px] h-4 w-3.5 rounded-[50%] bg-black text-[0px] focus-visible:outline-2 max-[530px]:fixed max-[530px]:left-[180px] max-[530px]:top-[362px] sm:left-[-440px] sm:top-[-38px]"
         to="#"
       >
         link
       </Link>
-      <button className="bg-white hover:shadow">hello</button>
     </div>
   );
 };
