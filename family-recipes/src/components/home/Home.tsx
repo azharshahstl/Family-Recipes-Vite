4dde99593aefc3f3a8510fd04d556cad8247e0cf
Azhar Shah <azharshahsaintlouis@gmail.com>
2025-03-20 11:24:17 +0000

    add and style log in links in modal

--- family-recipes/src/components/home/Home.tsx
+++ family-recipes/src/components/home/Home.tsx
@@ -1,23 +1,30 @@
-import { useState } from "react";
+import { useRef } from "react";
 import LoginAndSignup from "../login-and-signup/LoginAndSignup";
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const modalRef = useRef<HTMLDialogElement>(null);
 
-  const openModal = () => {
-    setIsOpen(true);
+  const toggleModal = () => {
+    if (!modalRef.current) return;
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    modalRef.current.hasAttribute("open")
+      ? modalRef.current.close()
+      : modalRef.current.showModal();
   };
+
   return (
     <div className="flex h-lvh w-full items-center justify-center bg-[url(../../public/paper.jpg)]">
       <div className="italianno-sm sm:text-9xl">Family Recipes</div>
       <button
-        onClick={openModal}
-        className="group relative left-[-333px] top-[-32px] h-3.5 w-3.5 overflow-hidden rounded-full bg-black px-4 py-4 text-white transition-all duration-300 ease-out hover:cursor-pointer hover:bg-gradient-to-r hover:from-gray-600 hover:to-gray-400 hover:ring-2 hover:ring-gray-600 hover:ring-offset-2 focus-visible:outline-offset-2 max-[530px]:fixed max-[530px]:left-[180px] max-[530px]:top-[362px] sm:left-[-440px] sm:top-[-38px]"
+        aria-label="Open sign up/in modal"
+        onClick={toggleModal}
+        className="group relative left-[-333px] top-[-32px] h-3.5 w-3.5 cursor-pointer overflow-hidden rounded-full bg-black text-white hover:bg-gradient-to-r hover:from-gray-600 hover:to-gray-400 hover:ring-2 hover:ring-gray-600 hover:ring-offset-2 max-[530px]:fixed max-[530px]:left-[180px] max-[530px]:top-[362px] sm:left-[-440px] sm:top-[-38px]"
       >
         <span className="ease absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 transform bg-white opacity-10 transition-all duration-700 group-hover:-translate-x-40"></span>
         <span className="relative"></span>
       </button>
-      <LoginAndSignup modalIsOpen={isOpen} toggleModal={setIsOpen} />
+      <LoginAndSignup ref={modalRef} toggleModal={toggleModal} />
     </div>
   );
 };
--- family-recipes/src/components/login-and-signup/LoginAndSignup.tsx
+++ family-recipes/src/components/login-and-signup/LoginAndSignup.tsx
@@ -1,29 +1,95 @@
-import { useRef } from "react";
+import { Link } from "react-router-dom";
 
 interface LoginAndSignupProps {
-  modalIsOpen: boolean;
-  toggleModal: React.Dispatch<React.SetStateAction<boolean>>;
+  ref?: React.Ref<HTMLDialogElement>;
+  toggleModal: () => void;
 }
 
-const LoginAndSignup = ({ modalIsOpen, toggleModal }: LoginAndSignupProps) => {
-  const modelRef = useRef<HTMLDialogElement>(null);
-
-  if (modalIsOpen) {
-    console.log(modalIsOpen);
-    modelRef.current?.showModal();
-  }
+const LoginAndSignup = (props: LoginAndSignupProps) => {
+  const { ref, toggleModal } = props;
 
   return (
-    <dialog ref={modelRef}>
-      <div>Hello</div>
-      <button
-        onClick={() => {
-          modelRef.current?.close();
-          toggleModal(false);
-        }}
-      >
-        Azhar
-      </button>
+    <dialog
+      className="open:animate-fade-in-scale open:backdrop:animate-fade-in-scale bg-transparent backdrop:backdrop-blur-sm"
+      onClick={(e) => {
+        if (e.target === e.currentTarget) toggleModal();
+      }}
+      ref={ref}
+    >
+      <div className="flex flex-col p-3">
+        <div className="m-6 self-center rounded-md bg-yellow-200">
+          <Link
+            to="sign-in-with-google"
+            className="m-4 inline-flex text-sm text-gray-800 hover:animate-pulse hover:text-black dark:text-neutral-200 dark:hover:text-blue-500 dark:focus:text-blue-500"
+            onClick={toggleModal}
+            aria-label="Sign in wit g Mail"
+          >
+            Sign In with G-Mail
+            <svg
+              className="width-4 shrink-0"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="m15 18 6-6-6-6" />
+            </svg>
+          </Link>
+        </div>
+        <div className="m-6 self-center rounded-md bg-yellow-200">
+          <Link
+            to="sign-in-with-google"
+            className="m-4 inline-flex self-center text-sm text-gray-800 hover:animate-pulse hover:text-black dark:text-neutral-200 dark:hover:text-blue-500 dark:focus:text-blue-500"
+            onClick={toggleModal}
+            aria-label="Sign in with e mail"
+          >
+            Sign In with E-Mail
+            <svg
+              className="width-4 shrink-0"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="m15 18 6-6-6-6" />
+            </svg>
+          </Link>
+        </div>
+        <div className="m-6 self-center rounded-md bg-yellow-200">
+          <Link
+            to="sign-in-with-google"
+            className="m-4 inline-flex self-center text-sm text-gray-800 hover:animate-pulse hover:text-black focus:text-blue-600 dark:text-neutral-200 dark:hover:text-blue-500 dark:focus:text-blue-500"
+            onClick={toggleModal}
+            aria-label="Create email account"
+          >
+            Create E-Mail Account
+            <svg
+              className="width-4 shrink-0"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="m15 18 6-6-6-6" />
+            </svg>
+          </Link>
+        </div>
+      </div>
     </dialog>
   );
 };
