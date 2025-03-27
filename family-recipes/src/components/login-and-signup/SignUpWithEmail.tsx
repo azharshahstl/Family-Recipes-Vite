87ea2156fbe12ef2ee0cf4330ee67be29fcd4928
Azhar Shah <azharshahsaintlouis@gmail.com>
2025-03-27 14:31:03 +0000

    chore: add min password req text

--- family-recipes/src/components/login-and-signup/SignUpWithEmail.tsx
+++ family-recipes/src/components/login-and-signup/SignUpWithEmail.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useRef, useState } from "react";
 import { auth } from "../../config/firebase.ts";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 const SignUpWithEmail = () => {
   const emailRef = useRef<HTMLInputElement>(null);
 
-  const passwordRef = useRef<HTMLInputElement>(null);
+  const [password, setPassword] = useState<string>("");
 
   const navigate = useNavigate();
 
@@ -15,8 +15,6 @@ const SignUpWithEmail = () => {
     e.preventDefault();
 
     const email = emailRef.current!.value;
-
-    const password = passwordRef.current!.value;
 
     try {
       await createUserWithEmailAndPassword(auth, email, password);
@@ -27,34 +25,55 @@ const SignUpWithEmail = () => {
     }
   };
 
+  const handleUpdatePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleRenderHelperText = () => {
+    if (password.length < 6)
+      return (
+        <div className="mb-3 text-[12px] text-red-700">
+          password must be at least 6 characters long
+        </div>
+      );
+    else {
+      return (
+        <div className="mb-3 text-[12px] text-green-700">
+          minimum password length met
+        </div>
+      );
+    }
+  };
+
   return (
     <div className="h-screen bg-gray-50">
       <div className="relative top-50">
         <form className="flex justify-center align-middle" onSubmit={signUp}>
           <div className="mt-4 mr-4 ml-4 flex w-52 flex-col sm:mr-20 sm:ml-20">
             <h1 className="text-center">Sign up with email</h1>
-            <br />
+
             <input
               aria-label="email"
               autoComplete="true"
-              className="rounded-b-xs border-1 border-amber-300 p-1 hover:bg-amber-100 focus-visible:bg-amber-100 focus-visible:outline-amber-500"
+              className="mt-3 mb-3 rounded-xs border-1 border-amber-300 p-1 hover:bg-amber-100 focus-visible:bg-amber-100 focus-visible:outline-amber-500"
               placeholder="email"
               ref={emailRef}
               required
             />
-            <br />
             <input
               aria-label="password"
               autoComplete="true"
-              className="rounded-b-xs border-1 border-amber-300 p-1 hover:bg-amber-100 focus-visible:bg-amber-100 focus-visible:outline-amber-500"
+              className="mt-3 mb-1 rounded-xs border-1 border-amber-300 p-1 hover:bg-amber-100 focus-visible:bg-amber-100 focus-visible:outline-amber-500"
               placeholder="password"
-              ref={passwordRef}
+              onChange={(e) => {
+                handleUpdatePassword(e);
+              }}
               type="password"
               required
             />
-            <br />
+            {handleRenderHelperText()}
             <button
-              className="rounded-b-xs border-1 border-amber-300 bg-amber-100 hover:animate-bounce hover:bg-amber-300 focus-visible:outline-amber-500"
+              className="mt-3 mb-3 rounded-xs border-1 border-amber-300 bg-amber-100 hover:animate-bounce hover:bg-amber-300 focus-visible:outline-amber-500"
               type="submit"
             >
               create account
