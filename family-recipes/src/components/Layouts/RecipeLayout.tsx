a91ebb4558b61dfff72ce2989071b0f1b3e160b9
Azhar Shah <azharshahsaintlouis@gmail.com>
2025-04-03 13:47:43 +0100

    chore: create link component

--- family-recipes/src/components/Layouts/RecipeLayout.tsx
+++ family-recipes/src/components/Layouts/RecipeLayout.tsx
@@ -56,14 +56,12 @@ const RecipeLayout = () => {
                   className={({ isActive }) =>
                     [
                       "group",
-                      "mr-1",
                       "self-center",
                       "rounded-full border-1",
                       "border-none",
                       "p-2",
                       "text-md",
                       "uppercase",
-                      "ml-auto",
                       "focus-visible:outline-amber-500",
                       "sm:text-base",
                       isActive ? "bg-amber-400" : "",
@@ -82,7 +80,7 @@ const RecipeLayout = () => {
                       "self-center",
                       "rounded-full border-1",
                       "border-none",
-                      "mr-1",
+
                       "p-2",
                       "text-md",
                       "uppercase",
@@ -118,7 +116,7 @@ const RecipeLayout = () => {
                 </NavLink>
                 <button
                   onClick={handleSignOut}
-                  className="group mr-4 ml-auto text-[15px] sm:text-[20px]"
+                  className="group mr-auto ml-auto text-[15px] sm:text-[20px]"
                   type="button"
                 >
                   signout
@@ -128,20 +126,21 @@ const RecipeLayout = () => {
             </div>
           </section>
           {/* Desktop menu */}
-          <div className="hidden justify-center pt-5 align-middle sm:flex">
+          <div className="mb-4 hidden justify-between pt-5 align-middle sm:flex">
             <span className="italianno-sm ml-4 self-center text-4xl">FR</span>
             <NavLink
               to="/recipes/personal"
               className={({ isActive }) =>
                 [
                   "group",
-                  "mr-1",
                   "self-end",
                   "rounded-full border-1",
                   "border-none",
                   "p-2",
+                  "pl-[25px]",
                   "text-xs",
                   "ml-auto",
+                  "mx-2",
                   "focus-visible:outline-amber-500",
                   "sm:text-base",
                   isActive ? "bg-amber-400" : "",
@@ -159,7 +158,7 @@ const RecipeLayout = () => {
                   "self-end",
                   "rounded-full border-1",
                   "border-none",
-                  "mr-1",
+                  "mx-2",
                   "p-2",
                   "text-xs",
                   "focus-visible:outline-amber-500",
@@ -181,7 +180,7 @@ const RecipeLayout = () => {
                   "border-none",
                   "p-2",
                   "text-xs",
-
+                  "ml-2",
                   "focus-visible:outline-amber-500",
                   "sm:text-base",
                   isActive ? "bg-amber-400" : "",
@@ -207,29 +206,12 @@ const RecipeLayout = () => {
             Search
           </label>
           <div className="relative w-full">
-            <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3">
-              <svg
-                className="h-4 w-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 18 20"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2"
-                />
-              </svg>
-            </div>
+            <div className="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3"></div>
             <input
               type="text"
               id="simple-search"
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 ps-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-              placeholder="Search branch name..."
-              required
+              className="block w-full rounded-full border border-gray-300 bg-gray-50 p-2.5 ps-10 text-sm text-gray-900 focus-visible:bg-amber-100 focus-visible:outline-amber-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              placeholder="Search title..."
             />
           </div>
           <button
@@ -245,9 +227,9 @@ const RecipeLayout = () => {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
               />
             </svg>
---
+++ family-recipes/src/components/ui/links/ForRecipePage.tsx
@@ -0,0 +1,39 @@
+import { NavLink } from "react-router-dom";
+
+interface RecipePageLinkProps {
+  handleOnClick?: () => void;
+  content: string;
+  goTo: string;
+  mobile?: boolean;
+}
+
+const RecipePageLink = (props: RecipePageLinkProps) => {
+  const { handleOnClick = () => null, content, goTo, mobile = true } = props;
+
+  return (
+    <NavLink
+      onClick={handleOnClick}
+      to={goTo}
+      className={({ isActive }) =>
+        [
+          "group",
+          "mr-1",
+          "self-center",
+          "rounded-full border-1",
+          "border-none",
+          "p-2",
+          mobile ? "text-md" : "text-xs",
+          mobile ? "uppercase" : "",
+          "ml-auto",
+          "focus-visible:outline-amber-500",
+          "sm:text-base",
+          isActive ? "bg-amber-400" : "",
+        ].join(" ")
+      }
+    >
+      {content}
+      <span className="block h-0.5 max-w-0 bg-amber-600 transition-all duration-600 group-hover:max-w-full"></span>
+    </NavLink>
+  );
+};
+export default RecipePageLink;
